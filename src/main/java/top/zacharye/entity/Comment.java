b4a1d5e60e35597f16da5b47855f2376ac3a5206
Zacharyye <zhouyin1994@hotmail.com>
2019-02-03 12:35:36 +0800

    improve some bugs

---
+++ src/main/java/top/zacharye/entity/Comment.java
@@ -0,0 +1,110 @@
+package top.zacharye.entity;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class Comment implements Serializable {
+    private Integer id;
+    private Integer article_id;
+    private Integer user_id;
+    private String content;
+    private Timestamp created_at;
+    private String created_time;
+    private Timestamp updated_at;
+    private String updated_time;
+    private Integer start;
+    private Integer pageSize;
+
+    //另添加参数
+    private String author;
+    private String email;
+    private String url;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getArticle_id() {
+        return article_id;
+    }
+
+    public void setArticle_id(Integer article_id) {
+        this.article_id = article_id;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getCreated_time() {
+        return created_time;
+    }
+
+    public void setCreated_time(String created_time) {
+        this.created_time = created_time;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public String getUpdated_time() {
+        return updated_time;
+    }
+
+    public void setUpdated_time(String updated_time) {
+        this.updated_time = updated_time;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
